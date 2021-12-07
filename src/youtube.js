8121b5d402548e15fc3e415b49752f4f91180d90
thunderteammax <94621146+thunderteammax@users.noreply.github.com>
2021-12-07 18:20:53 +0530

    Create youtube.js

---
+++ src/youtube.js
@@ -0,0 +1,15 @@
+// Atributos da API do Youtube
+const TOKEN_youtube = ""
+
+const part = `id,snippet`
+const type = `video`
+const orderClassification = `date`
+const quantidadeDeVideos = `1`
+
+module.exports = {
+    TOKEN_youtube,
+    part,
+    type,
+    orderClassification,
+    quantidadeDeVideos
+}
