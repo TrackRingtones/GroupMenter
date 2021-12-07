53cc024c8ae078320e88e39160abba13acc72b78
thunderteammax <94621146+thunderteammax@users.noreply.github.com>
2021-12-07 18:19:38 +0530

    Create channels.js

---
+++ src/channels.js
@@ -0,0 +1,24 @@
+const channelsEducation = [
+
+]
+
+const channelsMusic = [
+
+]
+
+const channelsEntertainment = [
+    
+]
+
+const lastVideoIdEducation = []
+const lastVideoIdMusic = []
+const lastVideoIdEntertainment = []
+
+module.exports = {
+    channelsEducation,
+    channelsMusic,
+    channelsEntertainment,
+    lastVideoIdEducation,
+    lastVideoIdMusic,
+    lastVideoIdEntertainment
+}
